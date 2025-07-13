7d41ed4469e0b8667fc5fdde5ae749ce3ec0359d
Sheshathri <srishesha07@gmail.com>
2025-07-13 16:05:12 +0530

    second

--- src/componentss/heroSection/HeroSection.jsx
+++ src/componentss/heroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
     <div className="w-full h-[500px] bg-black">
       <img
         className="w-full h-full object-contain"
-        src="https://sdmntpreastus.oaiusercontent.com/files/00000000-5ab0-61f9-a1b2-8012cdea4408/raw?se=2025-07-13T09%3A34%3A32Z&sp=r&sv=2024-08-04&sr=b&scid=1e85cc90-cd42-5cac-9e51-05466f7ba8e0&skoid=789f404f-91a9-4b2f-932c-c44965c11d82&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-12T23%3A54%3A04Z&ske=2025-07-13T23%3A54%3A04Z&sks=b&skv=2024-08-04&sig=Daj4MtCEM/BS/WUNZbs1sKsdtg66EnoHZ8U4BgwYfVE%3D"
+        src="https://sdmntpreastus.oaiusercontent.com/files/00000000-5ab0-61f9-a1b2-8012cdea4408/raw?se=2025-07-13T11%3A20%3A31Z&sp=r&sv=2024-08-04&sr=b&scid=ffa0951a-f579-5330-a6c7-2ce6b310183c&skoid=b0fd38cc-3d33-418f-920e-4798de4acdd1&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-13T10%3A13%3A25Z&ske=2025-07-14T10%3A13%3A25Z&sks=b&skv=2024-08-04&sig=HUlXroBnfNnW5H2BqaDS0c5%2B4nFSCUcr/da8IhnTQ3I%3D"
         alt="Hero Banner"
       />
     </div>
