0a1fc81712040a3e38eae5111d285e5b0a0ac970
Sheshathri <srishesha07@gmail.com>
2025-07-13 20:34:21 +0530

    banner update

--- src/componentss/heroSection/HeroSection.jsx
+++ src/componentss/heroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
       <img
         className="w-full h-full object-contain"
         src="https://sdmntpreastus.oaiusercontent.com/files/00000000-5ab0-61f9-a1b2-8012cdea4408/raw?se=2025-07-13T11%3A20%3A31Z&sp=r&sv=2024-08-04&sr=b&scid=ffa0951a-f579-5330-a6c7-2ce6b310183c&skoid=b0fd38cc-3d33-418f-920e-4798de4acdd1&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-13T10%3A13%3A25Z&ske=2025-07-14T10%3A13%3A25Z&sks=b&skv=2024-08-04&sig=HUlXroBnfNnW5H2BqaDS0c5%2B4nFSCUcr/da8IhnTQ3I%3D"
-        alt="Hero Banner"
+        alt="Hero Section Banner"
       />
     </div>
   );
