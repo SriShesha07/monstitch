3a0e558d852578b3c7475af21cbbc3ae2d2a9b33
Sheshathri <srishesha07@gmail.com>
2025-07-21 19:35:21 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -38,16 +38,19 @@ export default function App() {
           <Route path="/contactus" element={<ContactUs />} />
           {/* <Route path="/*" element={<NoPage />} /> */}
           <Route path="/productinfo/:id" element={<ProductInfo />} />
-          <Route path="/cart" element={
-            <ProtectedRouteForUser>
-            <CartPage />
-            </ProtectedRouteForUser>
-            } />
-          <Route path="/allproduct" element={<AllProduct />} />
+          <Route
+            path="/cart"
+            element={
+              <ProtectedRouteForUser>
+                <CartPage />
+              </ProtectedRouteForUser>
+            }
+          />
+          {/* <Route path="/allproduct" element={<AllProduct />} /> */}
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-           <Route path="/category/:categoryname" element={<CategoryPage />} />
-         {/* <Route path="/user-dashboard" element={
+          {/* <Route path="/category/:categoryname" element={<CategoryPage />} /> */}
+          {/* <Route path="/user-dashboard" element={
             <ProtectedRouteForUser>
               <UserDashboard />
             </ProtectedRouteForUser>
@@ -67,22 +70,62 @@ export default function App() {
               <UpdateProductPage />
             </ProtectedRouteForAdmin>
           } /> */}
-           <Route path="/admin-dashboard" element={ <ProtectedRouteForAdmin><AdminDashboard /> </ProtectedRouteForAdmin>} />
-            <Route path="/add-product" element={<AddProductPage />}  />
-            <Route path="/update-product/:id"  element={<UpdateProductPage />} />
-          <Route path="/user-dashboard" element={<UserDashboard />} />
-         <Route path="/checkout" element={<CheckoutPage />} />
-         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-         <Route path="/shipping-policy" element={<ShippingPolicy />} />
-         <Route path="/cancellation-policy" element={<CancellationPolicy />} />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <ProtectedRouteForAdmin>
+                <AdminDashboard />
+              </ProtectedRouteForAdmin>
+            }
+          />
+          <Route
+            path="/add-product"
+            element={
+              <ProtectedRouteForAdmin>
+                <AddProductPage />
+              </ProtectedRouteForAdmin>
+            }
+          />
+          <Route
+            path="/update-product/:id"
+            element={
+              <ProtectedRouteForAdmin>
+                <UpdateProductPage />
+              </ProtectedRouteForAdmin>
+            }
+          />
+          {/* <Route path="/user-dashboard" element={<ProtectedRouteForAdmin><UserDashboard /></ProtectedRouteForAdmin>} /> */}
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRouteForUser>
+                <CheckoutPage />
+              </ProtectedRouteForUser>
+            }
+          />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/shipping-policy" element={<ShippingPolicy />} />
+          <Route path="/cancellation-policy" element={<CancellationPolicy />} />
           <Route path="/termsandconditions" element={<TermsAndConditions />} />
-          <Route path="/orderSummary" element={<OrderSummary />} />
-          <Route path="/myOrders" element={<MyOrders />} />
-<Route path="/aboutUs" element={<AboutUs />} />
-
-
+          <Route
+            path="/orderSummary"
+            element={
+              <ProtectedRouteForUser>
+                <OrderSummary />
+              </ProtectedRouteForUser>
+            }
+          />
+          <Route
+            path="/myOrders"
+            element={
+              <ProtectedRouteForUser>
+                <MyOrders />
+              </ProtectedRouteForUser>
+            }
+          />
+          <Route path="/aboutUs" element={<AboutUs />} />
         </Routes>
-         <Toaster/>
+        <Toaster />
       </Router>
     </MyState>
   );
